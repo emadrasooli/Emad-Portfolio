c23ba7ed1abd71c4472aae4a5fabbe26462562ca
M.Eamd <emadrasooli2004@gmail.com>
2024-09-16 15:13:30 +0430

    Refactoring-skill-section-add-infinite-slider

--- build/main.js
+++ build/main.js
@@ -2,6 +2,8 @@ const enterToggle = document.getElementById("enterToggle");
 const closeToggle = document.getElementById("closeToggle");
 const header = document.getElementById("header");
 const navbar = document.getElementById("navbar");
+const copy = document.getElementById("items-slide").cloneNode(true);
+document.getElementById("items").appendChild(copy);
 
 enterToggle.addEventListener("click", () => {
   navbar.style.display = "none";
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
       },
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+      animation: {
+        slide: "slide 50s linear infinite",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
